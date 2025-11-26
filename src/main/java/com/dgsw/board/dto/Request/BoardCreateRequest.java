0e7c4e9439072d7cea798535c43402cd06fb048a
leeyujin0527 <sksdldbwls0527@gmail.com>
2025-11-26 17:04:18 +0900

    Validation 적용 완료

--- src/main/java/com/dgsw/board/dto/Request/BoardCreateRequest.java
+++ src/main/java/com/dgsw/board/dto/Request/BoardCreateRequest.java
@@ -1,6 +1,8 @@
 package com.dgsw.board.dto.Request;
 
 import com.dgsw.board.entity.Board;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BoardCreateRequest {
 
+    @NotBlank(message = "제목은 필수입니다.")
+    @Size(max = 100, message = "제목은 최대 100자까지 가능합니다.")
     private String title;
+    @NotBlank(message = "본문은 필수입니다.")
     private String content;
     private String author;
     private LocalDateTime deadline;
